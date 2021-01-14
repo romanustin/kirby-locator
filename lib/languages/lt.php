1acc382a9a5d62d3f078cb1ca17acd7b75817450
romanustin <53936318+romanustin@users.noreply.github.com>
2021-01-14 15:31:48 +0200

    Create lt.php

---
+++ lib/languages/lt.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'locator.placeholder'    => 'Ieškoti vietos',
+	'locator.locate'         => 'Rasti',
+	'locator.collapse'       => 'Sumažinti',
+	'locator.latitude'       => 'Platuma',
+	'locator.longitude'      => 'Ilguma',
+	'locator.number'         => 'Numeris',
+	'locator.address'        => 'Adresas',
+	'locator.postcode'       => 'Pašto kodas',
+	'locator.city'           => 'Miestas',
+	'locator.country'        => 'Šalis',
+	'locator.empty'          => 'Nėra vietos, kurią galima būtų rodyti.',
+	'locator.empty_response' => 'Vieta nerasta. Pabandykite įrašyti daugiau detalių.',
+	'locator.error'          => 'Kažkas nepavyko. Bandykite vėl, arba praneškite administratoriui.',
+  'locator.reset'          => 'Reset',
+);
